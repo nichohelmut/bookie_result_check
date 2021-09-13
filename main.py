78b2c9ec14cba56905f3c5cf3e47855667c61de7
nicholasutikal <nicholasutikal@gmail.com>
2021-09-13 13:45:07 +0200

    basic set up

---
+++ main.py
@@ -0,0 +1,12 @@
+from processor.xgb_result_check import ResultCheck
+
+
+def main():
+    print("Start with bookie_xgb_analysis ms")
+    pp = ResultCheck()
+    #pp.xgb_fit_and_predict()
+    return print("Done with bookie_xgb_analysis ms")
+
+
+if __name__ == "__main__":
+    main()
